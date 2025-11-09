59276071dff26eefcacc6fa58e8e272a31927272
Twahidul Islam <twahidulislam2005@gmail.com>
2025-11-09 19:16:04 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ for (let i = 0; i < name.length; i++) {
 
 // })
 
-let staf = ["karim", "Rahim", "Saim", "Rabbi", "Siam"];
+let staf = ["karim", "Rahim", "Saim", "Riyad" "Rabbi", "Siam"];
 staf.map((items) => {
   // console.log(`wellcome ${items}`);
 });
